2f6f74862a7fbeca1cb04c19cc10f00f1a42e946
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-07-03 11:17:41 -0700

    Add RedisClient class with basic Redis operations (get, set, del) and connection check

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.myClient = createClient();   // Create a new Redis client
+    this.myClient.on('error', (error) => console.log(error));  // Log any errors
+  }
+
+  // Method to check if the Redis client is connected
+  isAlive() {
+    return this.myClient.connected;
+  }
+
+  // Method to get a value from Redis based on the key
+  async get(key) {
+    const getAsync = promisify(this.myClient.GET).bind(this.myClient);  // Promisify the GET command
+    return getAsync(key);  // Retrieve the value from Redis
+  }
+
+  // Method to set a value in Redis with an expiration time
+  async set(key, val, time) {
+    const setAsync = promisify(this.myClient.SET).bind(this.myClient);  // Promisify the SET command
+    return setAsync(key, val, 'EX', time);  // Set the value in Redis with an expiration time
+  }
+
+  // Method to delete a value from Redis based on the key
+  async del(key) {
+    const delAsync = promisify(this.myClient.DEL).bind(this.myClient);  // Promisify the DEL command
+    return delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
